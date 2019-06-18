0f8357abf6130f17936320c9eeba5ae312a80a5c
VanishingTacos <devonstuper@gmail.com>
2019-06-18 12:35:04 -0400

    Add files via upload

---
+++ NoYTinfoPanel.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name        NoYTinfoPanel
+// @version     1.o
+// @description Get rid of info panel on YT
+// @author      VanishingTacos
+// @namespace   https://www.reddit.com/user/VanishingTacos/
+// @include     https://www.youtube.com/watch?v=*
+// @run-at      document-idle
+// @grant       none
+// ==/UserScript==
+
+		var element = document.getElementById("clarify-box");
+    	element.parentNode.removeChild(element);
